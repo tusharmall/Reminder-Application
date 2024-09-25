2d7e034265b1741639e8fd3f9012ba18d92e345a
Tushar Mall <115815953+tusharmall@users.noreply.github.com>
2024-09-25 21:26:35 +0530

    Update ReminderRepository.java

--- reminderapp/src/main/java/com/example/reminderapp/repository/ReminderRepository.java
+++ reminderapp/src/main/java/com/example/reminderapp/repository/ReminderRepository.java
@@ -1,13 +1,4 @@
-//
-//package com.example.reminderapp.repository;
-//
-//import com.example.reminderapp.model.Reminder;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ReminderRepository extends JpaRepository<Reminder, Long> {
-//}
+
 package com.example.reminderapp.repository;
 
 import com.example.reminderapp.model.Reminder;
